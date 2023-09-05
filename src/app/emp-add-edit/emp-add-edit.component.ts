6bdf9e7e5e6dd85efca94967f6f0de734cda6b17
sedin-tushar <tushar@sedintechnologies.com>
2023-09-05 14:46:11 +0530

    employe add service and json-server

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { Dialog, DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,6 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss']
 })
 export class EmpAddEditComponent {
+  empForm: FormGroup;
+
   education: string[] = [
     '10 th',
     '12 th',
@@ -13,4 +18,34 @@ export class EmpAddEditComponent {
     'Graduate',
     'Post Graduate'
   ];
+
+  constructor(private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: DialogRef<EmpAddEditComponent>) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    })
+  }
+
+  onFormSubmit() {
+    if(this.empForm.valid){
+      this._empService.addEmployee(this.empForm.value).subscribe({
+        next: (val: any) => {
+          alert('Employe Added Successfully')
+          this._dialogRef.close();
+        },
+        error: (err: any) =>{
+          console.log(err);
+        },
+      })
+    }
+  }
 }
