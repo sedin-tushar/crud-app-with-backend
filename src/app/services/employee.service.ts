638cb1a4b5b3d858f5d87e024aacd52f7c0dd658
sedin-tushar <tushar@sedintechnologies.com>
2023-09-05 15:20:59 +0530

    core services for table

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,17 +3,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private _http: HttpClient) {}
 
-  constructor(private _http: HttpClient) { }
-
-  addEmployee(data: any): Observable<any>{
-    return this._http.post(' http://localhost:3000/employees', data)
+  addEmployee(data: any): Observable<any> {
+    return this._http.post('http://localhost:3000/employees', data);
   }
 
-    getEmployeeList(): Observable<any>{
-    return this._http.get(' http://localhost:3000/employees')
+  updateEmployee(id: number, data: any): Observable<any> {
+    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+  }
+
+  getEmployeeList(): Observable<any> {
+    return this._http.get('http://localhost:3000/employees');
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this._http.delete(`http://localhost:3000/employees/${id}`);
   }
 }
